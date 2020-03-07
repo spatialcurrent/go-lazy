9bdaf8e1b6cab5450019eab823100c8e033efbfa
pjdufour <pjdufour.dev@gmail.com>
2020-03-07 10:45:14 -0500

    lazy file

---
+++ pkg/lazy/LazyFile_test.go
@@ -0,0 +1,26 @@
+// =================================================================
+//
+// Copyright (C) 2020 Spatial Current, Inc. - All Rights Reserved
+// Released as open source under the MIT License.  See LICENSE file.
+//
+// =================================================================
+
+package lazy
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLazyFile(t *testing.T) {
+	f := NewLazyFile("testdata/doc.txt", os.O_RDONLY, 0)
+	out, err := ioutil.ReadAll(f)
+	assert.NoError(t, err)
+	assert.Equal(t, "hello world\n", string(out))
+	fileInfo, err := f.Stat()
+	assert.NoError(t, err)
+	assert.Equal(t, "doc.txt", fileInfo.Name())
+}
