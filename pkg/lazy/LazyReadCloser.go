9ef226345bd94223b2b2fe78e4ecf8df3e9d7f91
pjdufour <pjdufour.dev@gmail.com>
2020-09-12 11:52:22 -0400

    LazyReadCloser

---
+++ pkg/lazy/LazyReadCloser.go
@@ -0,0 +1,48 @@
+// =================================================================
+//
+// Copyright (C) 2020 Spatial Current, Inc. - All Rights Reserved
+// Released as open source under the MIT License.  See LICENSE file.
+//
+// =================================================================
+
+package lazy
+
+import (
+	"io"
+)
+
+type LazyReadCloser struct {
+	init       func() (io.ReadCloser, error)
+	readCloser io.ReadCloser
+	err        error
+}
+
+func (r *LazyReadCloser) Read(p []byte) (int, error) {
+	if r.err != nil {
+		return 0, r.err
+	}
+	if r.readCloser == nil {
+		readCloser, err := r.init()
+		if err != nil {
+			r.err = err
+			return 0, err
+		}
+		r.readCloser = readCloser
+	}
+	return r.readCloser.Read(p)
+}
+
+func (r *LazyReadCloser) Close() error {
+	if r.readCloser != nil {
+		return r.readCloser.Close()
+	}
+	return nil
+}
+
+func NewLazyReadCloser(init func() (io.ReadCloser, error)) *LazyReadCloser {
+	return &LazyReadCloser{
+		init:       init,
+		readCloser: nil,
+		err:        nil,
+	}
+}
